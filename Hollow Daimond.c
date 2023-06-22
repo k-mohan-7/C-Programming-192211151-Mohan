58e2b6f2e10ce7d4765b68ce6e71cd1c17c8f893
k-mohan-7 <120168677+k-mohan-7@users.noreply.github.com>
2023-06-22 12:18:24 +0530

    Create Hollow Daimond.c

---
+++ Hollow Daimond.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+void main()
+{
+	int n,i,j;
+	printf("enter the no.of rows");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n-i;j++)
+		{
+			printf("  ");
+		}
+		for(j=0;j<=i;j++)
+		{
+			if(j==0)
+			{
+			printf("* ");
+		}
+			else if(j>0)
+		{
+			printf("  ");
+		}
+		}
+		for(j=1;j<=i;j++)
+		{
+			if(j==i)
+			{
+			printf("* ");
+		}
+		else if(j>0)
+		{
+			printf("  ");
+		}
+		}
+		printf("\n");
+	}
+	for(i=0;i<=n;i++)
+	{
+		for(j=0;j<i;j++)
+		{
+			printf("  ");
+		}
+		for(j=0;j<=n-i;j++)
+		{
+			if(j==0)
+			{
+			printf("* ");
+		}
+		else if(j>0)
+		{
+			printf("  ");
+		}
+		}
+		for(j=1;j<=n-i;j++)
+		{
+			if(j==n-i)
+			{
+			printf("* ");
+		}
+			else if(j>0)
+		{
+			printf("  ");
+		}
+		}
+		printf("\n");
+	}
+}
